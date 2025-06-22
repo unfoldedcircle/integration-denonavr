7b07d19a3f23594248a0e7a645c259c950646cbb
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2025-06-22 13:36:40 +0200

    Wait for 2 seconds to acquire lock, skip if failed.

--- intg-denonavr/avr.py
+++ intg-denonavr/avr.py
@@ -526,7 +526,11 @@ class DenonDevice:
         if self._update_lock.locked() or not self._active or self._connecting:
             return
 
-        await self._update_lock.acquire()
+        try:
+            await asyncio.wait_for(self._update_lock.acquire(), timeout=2.0)
+        except asyncio.TimeoutError:
+            _LOG.info("[%s] Update lock timeout, skipping update", self.id)
+            return
 
         try:
             receiver = self._receiver
